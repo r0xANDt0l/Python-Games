70e5c5a5f715a98b6bafb4c57682cf8d883dc0ab
r0xANDt0l <41841903+r0xANDt0l@users.noreply.github.com>
2022-01-29 10:26:41 +0100

    Started working on the rpg

---
+++ GUI/RPG.py
@@ -0,0 +1,10 @@
+from unicodedata import name
+import arcade
+
+class MyGame(arcade.Window):
+    def __init__(self, width = 800, height = 600, windowName = "Ventana") -> None:
+        super().__init__(width, height, windowName)
+
+arcade.Window
+
+arcade.open_window
