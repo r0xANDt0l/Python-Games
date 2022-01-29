09b9be92034d11624f606f87d3e5bb4c3c179de3
r0xANDt0l <41841903+r0xANDt0l@users.noreply.github.com>
2022-01-29 10:35:11 +0100

    created more definitions on class MyGame

--- GUI/RPG.py
+++ GUI/RPG.py
@@ -5,6 +5,13 @@ class MyGame(arcade.Window):
     def __init__(self, width = 800, height = 600, windowName = "Ventana") -> None:
         super().__init__(width, height, windowName)
 
+
+    def on_update(self, delta_time: float):
+        pass
+
+    def on_draw(self):
+        pass
+
 arcade.Window
 
 arcade.open_window
